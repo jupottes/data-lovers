89c827c5a62c6f48a87ba31437976da077dafceb
Iana-Neri22 <iananeri@outlook.com>
2019-02-26 19:18:13 -0300

    funcao filtro mostrando na tela

--- src/main.js
+++ src/main.js
@@ -4,34 +4,13 @@ window.onload = function() {
   };
   
   const getPokemons =() => pokemons["pokemon"];
-  
-  
-  /*const showPokemons =() =>{
-  let pokemonList = document.getElementById("pokemons-div");
-  
-  pokemonList.innerHTML = `
-  ${getPokemons().map((pkm) => `
-    <div class="pkm-item">
-      <img src="${pkm["img"]}" class="pkm-img" />
-      <div class="text-price">
-        <p class="pkm-num">${pkm["num"]}</p>
-      </div>
-      <div class="text-name">
-        <h3 class="pkm-name">${pkm["name"]}</h3>
-      </div>
-      
-  </div>
-  `
-  ).join("")
-  }
-  `
 
-
-  }*/
+  let meusPokemons = getPokemons()
+  
   const showPokemons =() =>{
     let pokemonList = document.getElementById("pokemons-div");
     pokemonList.innerHTML =`
-    ${getPokemons().map((pkm) => `
+    ${meusPokemons.map((pkm) => `
       <div class="pkm-item">
         <img src="${pkm["img"]}" class="pkm-img" />
         <div class="text-price">
@@ -49,19 +28,33 @@ window.onload = function() {
     ` 
   }
 
-  
-  //Aqui ele está trazendo todos do tipo fogo
-  //O "tipo" dentro da função vai ser comparado de acordo com o valor do search*/
-  //Função funcionando
  const filterByType = () =>{
+meusPokemons = meusPokemons.filter(pkms => (pkms["type"][0] == "Fire"));
+showPokemons()
+  //console.log(filteredByType);
+}
+//console.log(filterByType());
 
-  let filteredByType = getPokemons().filter(pkms => (pkms["type"][0] == "Fire"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const filterByEgg = () =>{
+
+  let filteredByType = getPokemons().filter(pkms => (pkms["egg"][0] == "2"));
   return filteredByType
 }
-// console.log(filterByType());
-
  const sortByName = () =>{
  let sorted = getPokemons().sort(pkms => (pkms[name])
  console.log(sorted;
   }
-   
+   */
