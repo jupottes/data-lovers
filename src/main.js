c44865fda17e2f16d7b6f0362b55966c60f068e2
Iana-Neri22 <iananeri@outlook.com>
2019-02-27 18:04:11 -0300

    filtro funcionando para todos os tipos de pokemon

--- src/main.js
+++ src/main.js
@@ -33,12 +33,50 @@ window.onload = function() {
 
   // Função que filtra o pokemon por tipo
   //A variável tipo vai ser puxada do html
-const filterByType = () =>{
+/*const filterByType = () =>{
 meusPokemons = meusPokemons.filter(pkms => (pkms["type"][0] == "Fire"));
 showPokemons()
+}*/
+
+//Funcionando, Paloma
+
+const allCheck = document.querySelectorAll(".checkInput");
+console.log(allCheck) 
+for (btns of allCheck){
+  console.log(btns)
+ btns.addEventListener("click", function (e){let tipoPkm = e.target.id
+  meusPokemons = meusPokemons.filter(pkms => (pkms["type"][0] == tipoPkm))
+  // filterByType()
+})
 }
-const searchByType = document.querySelector("#typeSearch");
-searchByType.addEventListener("click", filterByType);
+
+const filterByType = () =>{
+  //let tipoPkm = document.getElementsByClassName("checkInput").value;
+   //console.log(tipoPkm)
+ 
+  //  meusPokemons = meusPokemons.filter(pkms => (pkms["type"][0] == tipoPkm));
+    showPokemons()
+    //console.log()
+    }
+
+
+
+
+/*const filterByType = () =>{
+ let tipoPkm = document.getElementsByClassName("typeSearch").value;
+ 
+  meusPokemons = meusPokemons.filter(pkms => (pkms["type"][0] == tipoPkm));
+  showPokemons()
+  }*/
+
+
+
+//Seleciona todos os checkbox pelo id da section
+  document.querySelector("#buscaPkm").addEventListener("click", filterByType)
+  
+
+
+
 
 //Essa filtra pelo ovo
 //Distancia percorrida necessária para o pokemon nascer 
@@ -48,6 +86,8 @@ const filterByEgg = () =>{
 showPokemons()
 }
 
+//document.getElementById("myInput1").addEventListener("keyup", myFunction);
+//searchByType.addEventListener("click", filterByType);
 
 //FUNÇÃO INPUT DA BUSCA
 function myFunction() {
@@ -55,7 +95,7 @@ function myFunction() {
   var input, filter, ul, li, a, i, txtValue;
   input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
-  ul = document.getElementById("myUL");
+  ul = document.getElementById("types");
   li = ul.getElementsByTagName("li");
   for (i = 0; i < li.length; i++) {
       a = li[i].getElementsByTagName("a")[0];
@@ -68,16 +108,30 @@ function myFunction() {
   }
 }
 
-let search = document.getElementById("myUL");
+/*let search = document.getElementById("types");
 search.style.display= "none";
 
+let search2 = document.getElementById("eggs");
+search.style.display= "none";*/
 
 
+function myFunction2() {
+  search.style.display= "block";
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById("myInputEgg");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("eggs");
+  li = ul.getElementsByTagName("li");
+  for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          li[i].style.display = "";
+      } else {
+          li[i].style.display = "none";
+      }
+  }
+}
 
-
-
-
-
-
-
-
+//let pokeType = document.getElementsByName("pokeType");
+//alert(pokeType[1].value);
