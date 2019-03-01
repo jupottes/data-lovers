a74f0e2ee68e2dad63039cd3a6f6b04ca01ad38a
jupottes <juliettepottes@gmail.com>
2019-03-01 14:16:09 -0300

    erro

--- src/main.js
+++ src/main.js
@@ -1,18 +1,22 @@
-window.onload = function() {
+window.onload = function () {
   showPokemons();
 
 };
 
-const getPokemons =() => pokemons["pokemon"];
+const getPokemons = () => pokemons["pokemon"];
 //Essa variável vai "guardar" as mudanças aplicadas pelos filtros
 //Sem ela os pokemons não são exibidos na tela
 let meusPokemons = getPokemons()
 
+const namesPkm = meusPokemons[0]["name"];
+console.log(namesPkm)
 
+let namePkm = "";
+for (let i = 0; i < .length; i++) {
 
-const showPokemons =() =>{
+const showPokemons = () => {
   let pokemonList = document.getElementById("pokemons-div");
-  pokemonList.innerHTML =`
+  pokemonList.innerHTML = `
   ${meusPokemons.map((pkm) => `
     <div class="pkm-item">
       <img src="${pkm["img"]}" class="pkm-img" />
@@ -28,18 +32,23 @@ const showPokemons =() =>{
   </div>
   `
   ).join("")
-  }
-  ` 
+    }
+  `
 }
 
+pkm.sort((a, b) => a - b);
+console.log(teste)
+
 const allCheck = document.querySelectorAll(".checkInput");
-for (btns of allCheck){
-btns.addEventListener("click", function (e){let tipoPkm = e.target.id
-meusPokemons = meusPokemons.filter(pkms => (pkms["type"][0] == tipoPkm || pkms["type"][1] == tipoPkm))})
+for (btns of allCheck) {
+  btns.addEventListener("click", function (e) {
+    let tipoPkm = e.target.id
+    meusPokemons = meusPokemons.filter(pkms => (pkms["type"][0] == tipoPkm || pkms["type"][1] == tipoPkm))
+  })
 }
 
 //Função que mandar mostrar os pokemons quando o filtro é aplicado
-const filterByType = () =>{
+const filterByType = () => {
   showPokemons()
 }
 
@@ -50,7 +59,7 @@ document.querySelector("#buscaPkm").addEventListener("click", filterByType)
 //Distancia percorrida necessária para o pokemon nascer 
 //2, 5 ou 10km
 //Precisa ser refatorada pra ficar igual a do filtro
-const filterByEgg = () =>{
-meusPokemons = meusPokemons.filter(pkms => (pkms["egg"] == "2 km"));
-showPokemons()
+const filterByEgg = () => {
+  meusPokemons = meusPokemons.filter(pkms => (pkms["egg"] == "2 km"));
+  showPokemons()
 }
